ef94311499303e3c4f2d1ef79959d7b0a18738d8
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-08-16 18:36:25 +0600

    HW2

--- app/src/main/java/com/example/my_applicationhw1/AnimalAdapter.kt
+++ app/src/main/java/com/example/my_applicationhw1/AnimalAdapter.kt
@@ -9,7 +9,7 @@ import com.example.my_applicationhw1.databinding.ItemAnimalBinding
 
 class AnimalAdapter:Adapter<AnimalAdapter.ViewHolder>() {
     val animalList = arrayListOf<AnimalModel>(
-        AnimalModel(title= "Ilgiz",desc= "0555253552", img = "https://static.vecteezy.com/system/resources/previews/057/068/323/non_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"),
+        AnimalModel(title= "Ilgiz",desc= "0990442484", img = "https://static.vecteezy.com/system/resources/previews/057/068/323/non_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"),
         AnimalModel(title= "Aktilek",desc= "0555253552", img = "https://static.vecteezy.com/system/resources/previews/057/068/323/non_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"),
         AnimalModel(title= "Asel",desc= "0555253552", img = "https://static.vecteezy.com/system/resources/previews/057/068/323/non_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"),
         AnimalModel(title= "Aida",desc= "0555253552", img = "https://static.vecteezy.com/system/resources/previews/057/068/323/non_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"),
@@ -38,9 +38,9 @@ class AnimalAdapter:Adapter<AnimalAdapter.ViewHolder>() {
 
     class ViewHolder(private val binding: ItemAnimalBinding): RecyclerView.ViewHolder(binding.root){
         fun onBind(animalModel:AnimalModel){
-            binding.tvTitle.text = animalModel.title
-            binding.tvDesc.text = animalModel.desc
-            Glide.with(binding.ivPhoto).load(animalModel.img).into(binding.ivPhoto);
+            binding.tvTitile.text = animalModel.title
+            binding.descTitile.text = animalModel.desc
+            Glide.with(binding.tvImage).load(animalModel.img).into(binding.tvImage);
 
 
         }
